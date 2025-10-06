0d2b0221128b69d1296ab8f3d5f042271591f909
KhanhDeddo <khanhdeddo@gmail.com>
2025-10-07 03:51:05 +0700

    update chat bot

--- src/components/clients/chatbot/index.tsx
+++ src/components/clients/chatbot/index.tsx
@@ -1,48 +1,48 @@
 "use client";
-import React from "react";
-import axios from "axios";
-const apiUrl = import.meta.env.VITE_API_URL;
+// import React from "react";
+// import axios from "axios";
+// const apiUrl = import.meta.env.VITE_API_URL;
 import ChatToggle from "./components/ChatToggle";
 import useToggle from "@/hooks/common/useToggle";
 import ChatBox from "./components/ChatBox";
-type Message = {
-  sender: "user" | "genimi";
-  text: string;
-};
+// type Message = {
+//   sender: "user" | "genimi";
+//   text: string;
+// };
 
 export default function ChatBot() {
   const { value: isOpen, toggle, off } = useToggle();
-  const [message, setMesage] = React.useState("");
-  const [isloading, setIsloading] = React.useState(false);
-  const [arr, setArr] = React.useState<Message[]>([
-    {
-      sender: "genimi",
-      text: "Chào bạn! :v Mình có thể giúp gì cho bạn hôm nay? 😎",
-    },
-  ]);
-  const askGenimi = async () => {
-    const res = await axios.post(`${apiUrl}/ask-gemini`, {
-      message: message,
-    });
-    await setIsloading(false);
-    setArr((prev) => [
-      ...prev,
-      {
-        sender: "genimi",
-        text:
-          res?.data ??
-          "Không có phản hồi, vui lòng hỏi lại hoặc nhập yêu cầu khác 😙😙😙 ",
-      },
-    ]);
-  };
-  const updateArr = (message: string) => {
-    setIsloading(true);
-    setArr((prev) => [...prev, { sender: "user", text: message }]);
-    if (message === "clear" || message === "Clear") setArr([]);
-    askGenimi();
-    setMesage("");
-  };
-  console.log(arr);
+  // const [message, setMesage] = React.useState("");
+  // const [isloading, setIsloading] = React.useState(false);
+  // const [arr, setArr] = React.useState<Message[]>([
+  //   {
+  //     sender: "genimi",
+  //     text: "Chào bạn! :v Mình có thể giúp gì cho bạn hôm nay? 😎",
+  //   },
+  // ]);
+  // const askGenimi = async () => {
+  //   const res = await axios.post(`${apiUrl}/ask-gemini`, {
+  //     message: message,
+  //   });
+  //   await setIsloading(false);
+  //   setArr((prev) => [
+  //     ...prev,
+  //     {
+  //       sender: "genimi",
+  //       text:
+  //         res?.data ??
+  //         "Không có phản hồi, vui lòng hỏi lại hoặc nhập yêu cầu khác 😙😙😙 ",
+  //     },
+  //   ]);
+  // };
+  // const updateArr = (message: string) => {
+  //   setIsloading(true);
+  //   setArr((prev) => [...prev, { sender: "user", text: message }]);
+  //   if (message === "clear" || message === "Clear") setArr([]);
+  //   askGenimi();
+  //   setMesage("");
+  // };
+  // console.log(arr);
   return (
     <>
       <ChatToggle toggle={toggle} isOpen={isOpen} />
