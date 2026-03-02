f8e0513f7f7926c537203c5bc29f2b3060270172
Văn Khanh <khanhdev1902@gmail.com>
2026-03-02 23:22:18 +0700

    fix cros for client

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,7 +6,12 @@ import cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://192.168.1.38:5173'],
+    origin: [
+      'http://localhost:5173',
+      'http://192.168.1.38:5173',
+      'http://localhost:4173',
+      'https://luxhouse.vercel.app',
+    ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
